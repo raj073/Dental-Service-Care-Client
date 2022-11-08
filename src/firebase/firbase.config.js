a0cd2f5661a45f425ac902bc9fd059111921a794
raj073 <rajoni.das@outlook.com>
2022-11-08 11:09:00 +0600

    first commit

---
+++ src/firebase/firbase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCaFaKJG1kpkStHLAjsPGn2eP7UfayhSrk",
+    authDomain: "dental-care-services-afaee.firebaseapp.com",
+    projectId: "dental-care-services-afaee",
+    storageBucket: "dental-care-services-afaee.appspot.com",
+    messagingSenderId: "596079884397",
+    appId: "1:596079884397:web:e8518d030b27a1971f6764"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
